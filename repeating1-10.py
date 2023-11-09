739039faa15d3587710db5289f5ebb0cdf2a9cf4
dominicktan <145531805+dominicktan@users.noreply.github.com>
2023-11-09 17:05:16 +0800

    Add files via upload

---
+++ repeating1-10.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov  9 15:50:02 2023
+
+@author: dominicktan
+"""
+x = 1
+while x < 11:
+    print(x)
+    x = x+1
+    if x == 11:
+        x = 1
+
+
